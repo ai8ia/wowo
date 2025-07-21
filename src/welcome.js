3330ec4838350450f771a224ac2d6beef855e12c
ai8ia <ai8ia.win@gmail.com>
2025-07-22 02:13:03 +0800

    Update welcome.js

--- src/welcome.js
+++ src/welcome.js
@@ -1,1 +1,9 @@
+window.onload = () => {
+  const message = new SpeechSynthesisUtterance();
+  message.text = "三、二、一，致富引擎啟動。探索者，歡迎登艦。";
+  message.lang = "zh-TW";
+  message.rate = 1;
+  message.pitch = 1.1;
+  speechSynthesis.speak(message);
+};
 
